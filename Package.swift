cab0a9a3d1ad73de15b8c5cb051eb217151283e2
Tozik <tozika@inbox.ru>
2024-12-10 14:30:46 +0000

    Tun2SocksKit Release 4.7.11!!!

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/tozik/Tun2Socks-tozik/releases/download/4.7.10/HevSocks5Tunnel.xcframework.zip",
-        checksum: "71bb789042cde628601adaf24c03671ac6cd9edd8571b6b5b0c471a0f97fbaa5"
+        url: "https://github.com/tozik/Tun2Socks-tozik/releases/download/4.7.11/HevSocks5Tunnel.xcframework.zip",
+        checksum: "be7d88d7f506a20e60552e5d076cffc8fe93869249f927e617904293f77046dc"
     )
   ]
 )
