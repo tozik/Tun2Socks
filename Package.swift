d4d7eac52e706e4eb57e0b2c6ef06d5e7f71d660
GitHub Action <action@github.com>
2025-08-24 20:43:33 +0000

    Update Package.swift for version 4.7.32 from branch ofuscating

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "NetworkTunnelProcessor",
-      url: "https://github.com/tozik/Tun2Socks/releases/download/4.7.31/NetworkTunnelProcessor.xcframework.zip",
-      checksum: "f742f0f05f4158b37a7d82730c1d564b781854933a91578640b5ac87ce1b1219"
+      url: "https://github.com/tozik/Tun2Socks/releases/download/4.7.32/NetworkTunnelProcessor.xcframework.zip",
+      checksum: "bb5f546f6f6f510f2ae746dda398918da6c0f3c32bb823c4bd9db04a07e3c457"
     )
   ]
 )
