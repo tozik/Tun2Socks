001f881ce436e23832c9f277b62899ccc5a2245e
GitHub Action <action@github.com>
2025-11-27 11:40:13 +0000

    Update Package.swift for version 4.141 from branch ofuscating

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "HevSocks5Tunnel",
-      url: "https://github.com/tozik/Tun2Socks/releases/download/4.7.33/VPNSTrafficManager.xcframework.zip",
-      checksum: "b91ccf55f896e035ebd8e9442381ef3b2d9fe966013d0a2a3d78ec248ea91977"
+      url: "https://github.com/tozik/Tun2Socks/releases/download/4.141/VPNSTrafficManager.xcframework.zip",
+      checksum: "8dea3367d818560be507f66e9fff9f069f582646a4ef94374be1dc681aa7da8a"
     )
   ]
 )
